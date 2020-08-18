2eca2b25814ff36f562d0742159ede54257b0657
Shevaughn Grant <gshevaughn@gmail.com>
2020-08-18 19:22:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase) {
-    return("I can't hear you!")
+    return ("I can't hear you!")
+  } else if (string.toUpperCase) {
+    return ("YES INDEED!")
+  } else {
+    return ("I love you too")
   }
 }
 
